82bbe8c16fe0a6b88ba9b94798fcedf3abe47c0c
Aryan Hudda <aryanhudda1643@gmail.com>
2025-08-06 14:53:53 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  projectId: "podosi-cafe-online",
+  appId: "1:1056741447991:web:0350e11d1db452ae7722b2",
+  storageBucket: "podosi-cafe-online.firebasestorage.app",
+  apiKey: "AIzaSyD4PRNIuGzfxkQVlSPfQGddJgF4eiTG1Dc",
+  authDomain: "podosi-cafe-online.firebaseapp.com",
+  messagingSenderId: "1056741447991"
 };
 
 // Initialize Firebase
